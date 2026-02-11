72a48e539a59bc273e56897a7119f9b2815f91cd
NISAWEBAPP <nisainmobiliaria59@gmail.com>
2026-02-11 10:27:12 -0300

    Change fill color for 'Recuperado Libre' style

--- styles/Combinado_3_style.js
+++ styles/Combinado_3_style.js
@@ -54,7 +54,7 @@ case 'Cancelado':
                     break;
 case 'Recuperado Libre':
                     return [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.575)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(18,255,69,0.575)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.575)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,148,11,0.575)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
