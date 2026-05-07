11e4907f07b512723becfe536375b79bf4d9d611
NISAWEBAPP <nisainmobiliaria59@gmail.com>
2026-05-07 10:52:38 -0300

    Update functions.js

--- resources/functions.js
+++ resources/functions.js
@@ -23,6 +23,7 @@ var createTextStyle = function(feature, resolution, labelText, labelFont,
         offsetX: 0,
         offsetY: 0,
         placement: placement,
+        overflow: true,
         maxAngle: 0,
         fill: new ol.style.Fill({
           color: labelFill
