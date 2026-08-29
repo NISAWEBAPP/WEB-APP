f00f564cb89a982047a52f316ce215e7b0ab41ac
NISAWEBAPP <nisainmobiliaria59@gmail.com>
2026-08-29 06:33:08 -0300

    Add files via upload

---
+++ layers/fraccion_5.js
@@ -0,0 +1,805 @@
+var json_fraccion_5 = {
+  type: "FeatureCollection",
+  name: "fraccion_5",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: { FRACCION: "68" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.841960615930049, -25.278063941161935],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "17" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.941956078957347, -25.461667281316537],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "18" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.957926195365339, -25.388365158380971],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "23" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.90994887270147, -25.571025097595125],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "25" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.784659269318865, -25.514969575691833],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "29" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.40724420889309, -25.489075560307526],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "30" },
+      geometry: {
+        type: "Point",
+        coordinates: [-55.99027789273935, -25.499266795114824],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "31" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.307800027275761, -25.101462348917952],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "33" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.000357848913282, -25.543099101868876],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "4" },
+      geometry: {
+        type: "Point",
+        coordinates: [-59.837091504774818, -22.570749687293791],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "5" },
+      geometry: {
+        type: "Point",
+        coordinates: [-59.833540521282337, -22.577939774329511],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "34" },
+      geometry: {
+        type: "Point",
+        coordinates: [-59.821502209532021, -22.570146820417271],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "8" },
+      geometry: {
+        type: "Point",
+        coordinates: [-59.832308425552554, -22.583003120209455],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "36" },
+      geometry: {
+        type: "Point",
+        coordinates: [-55.367170116301452, -25.409262912276237],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "35" },
+      geometry: {
+        type: "Point",
+        coordinates: [-55.367419609605051, -25.410690176246362],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "37" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.778273638453513, -25.427375916194396],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "38" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.300714451178131, -25.191294652578428],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "39" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.903944470168383, -25.523486200868806],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "42" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.908053696173496, -25.523054176621248],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "24" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.911499830308436, -25.57154979300136],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "40" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.909191443604932, -25.573850708232928],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "41" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.884043256372784, -25.537772931822229],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "44" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.856216610488609, -25.541527612185511],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "43" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.854342980989436, -25.542033714337169],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "47" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.962066730637162, -25.23430913723455],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "50" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.069271557809863, -25.332797545062856],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "55" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.838228599870192, -25.629754278877371],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "57" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.106314523967797, -25.280647053276084],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "59" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.103304303104061, -25.282627507326083],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "2" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.071260256657133, -25.617934882928267],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "67" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.818620771224964, -25.892750254844199],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "69" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.053450931398267, -25.616338547184998],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "9" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.477257387576479, -25.222952989581096],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "3" },
+      geometry: {
+        type: "Point",
+        coordinates: [-55.829366785798719, -27.271472740642992],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "13" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.373088151795322, -26.139677648430471],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "15" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.377094138281011, -24.612558715571846],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "14" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.37676722405714, -24.614906008999622],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "1" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.366688939321335, -25.116884293337058],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "16" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.537452888708941, -24.960689180883715],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "71" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.307454108303453, -25.099884221023075],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "72" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.848555255962992, -25.257066487588411],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "75" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.927565079117059, -25.477907661964228],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "76" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.935324095110815, -25.402634218783916],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "78" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.863290593425972, -24.498457632036583],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "79" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.018508742091619, -25.642094714260551],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "81" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.907771947095227, -25.398328122704296],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "82" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.90872215687137, -25.400996279219292],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "83" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.049537969458079, -25.619398826342977],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "89" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.870014682957567, -25.440873988133326],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "19" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.948549291115143, -25.379492981235998],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "28" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.812844619464684, -25.481262139976238],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "26" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.810135411373381, -25.485095835994954],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "27" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.810135411373381, -25.481087878161439],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "45" },
+      geometry: {
+        type: "Point",
+        coordinates: [-55.317287990117883, -25.436715194170731],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "46" },
+      geometry: {
+        type: "Point",
+        coordinates: [-55.991079159008521, -25.502184466915896],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "49" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.917491663036635, -25.461306785106824],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "58" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.109457971866249, -25.281523758518134],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "60" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.07070190461549, -25.325561422967656],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "61" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.075804715440604, -25.543754625120187],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "62" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.729219158434944, -25.468172776502968],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "64" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.672847111841975, -25.54979313079312],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "65" },
+      geometry: {
+        type: "Point",
+        coordinates: [-55.160124619721898, -25.439528736387373],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "66" },
+      geometry: {
+        type: "Point",
+        coordinates: [-55.161815602053466, -25.437959396267296],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "73" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.748519439676187, -25.526616011620675],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "80" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.941963968742044, -25.378883946648447],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "87" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.845633865613401, -25.635968618137856],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "88" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.348203362569635, -24.575019861471386],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "52" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.542007102606235, -24.963462748092962],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "53" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.883920845358425, -25.303848471296313],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "91" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.484940296762197, -24.189588710958748],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "44" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.494645703652182, -24.180374669628513],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "63" },
+      geometry: {
+        type: "Point",
+        coordinates: [-58.268612323175738, -26.865114011179024],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "74" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.09105398771505, -25.34131114974987],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "32" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.283638865630664, -25.253818187775781],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "84" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.737882090150364, -25.487811896444068],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "70" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.744220397168803, -25.512029765764655],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "77" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.864787007069616, -25.887438502741354],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "7" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.657200321112477, -25.437378698995378],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "21" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.818274451305598, -25.505064072355882],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "22" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.816403223799384, -25.503060135066502],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "11" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.75029082781743, -25.512217191161419],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "85" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.75223859785919, -25.544868938267768],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "94" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.70910657738365, -25.471249953164826],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "95" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.712716307853761, -25.470997286275193],
+      },
+    },
+   
+    {
+      type: "Feature",
+      properties: { FRACCION: "92" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.332724980463873, -25.142836938455719],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "93" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.331784966198434, -25.145316822436715],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "10" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.746936181476293, -25.462853787295909],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "20" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.751258847833164, -25.513131920390002],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "51" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.753393932307816, -25.545204663433829],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "56" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.743462350547063, -25.512276113261468],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "86" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.699255301514476, -25.447926469653378],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "48" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.957780342513523, -25.100206385832635],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "54" },
+      geometry: {
+        type: "Point",
+        coordinates: [-54.82081816495301, -25.502947344091787],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "97" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.417827874802398, -24.226955660128425],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "96" },
+      geometry: {
+        type: "Point",
+        coordinates: [-55.375784048720377, -25.408279913341161],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "98" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.437577896739057, -24.241588470568118],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "99" },
+      geometry: {
+        type: "Point",
+        coordinates: [-55.373422727273748, -25.406297079524236],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "100" },
+      geometry: {
+        type: "Point",
+        coordinates: [-55.370433076618014, -25.405366541718113],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { FRACCION: "101" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.264730419416381, -26.05618747049115],
+      },
+    },
+  ],
+};
+
+
+
+
+
